053fc7812af091ec310a7423f809b154e5e819b7
Matt Hink <mhink1103@gmail.com>
2011-11-13 04:26:08 -0600

    Began work on DecisionTree and wrote a spec for entropy calculation.

--- lib/aiproject3/decision_tree.rb
+++ lib/aiproject3/decision_tree.rb
@@ -1,2 +1,22 @@
+require File.dirname(__FILE__) + '/dataset_reader'
+
 class DecisionTree
+  
+  def initialize( dataset )
+    @dataset = dataset
+    @children = []
+  end
+
+  def choose_best_attribute
+    
+  end
+
+  def calculate_information_gain( attribute )
+    
+  end
+
+  def calculate_current_entropy
+   nil
+  end
+  
 end
--- spec/decision_tree_spec.rb
+++ spec/decision_tree_spec.rb
@@ -1,6 +1,17 @@
 require File.dirname(__FILE__) + '/spec_helper'
+require 'aiproject3/dataset_reader'
 require 'aiproject3/decision_tree'
 
 describe DecisionTree do
-  it "should represent a decision tree" 
+  before(:each) do
+     spec_directory = File.dirname(__FILE__)
+     @dataset_reader = DatasetReader.new(
+      (spec_directory + '/testcases/testcase.data'),
+      20, true)
+     @test_decision_tree = DecisionTree.new(@dataset_reader)
+  end
+
+  it "should calculate current entropy correctly" do
+    @test_decision_tree.calculate_current_entropy.should == 0.5
+  end
 end
