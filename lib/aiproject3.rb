351f291a510c4c42a2c380ad6dfa9ae5fe82f1de
Matt Hink <mhink1103@gmail.com>
2011-11-17 07:38:38 -0600

    Final commit. Everything working okay.

--- lib/aiproject3.rb
+++ lib/aiproject3.rb
@@ -1,6 +1,5 @@
 require './lib/aiproject3/dataset.rb'
 require './lib/aiproject3/decision_tree.rb'
-require 'pp'
 
 testing = (ARGV[1]=="test")
 dataset = Dataset.new :data_filename => ARGV[0], :testing => testing
@@ -10,7 +9,7 @@ dataset = Dataset.new :data_filename => ARGV[0], :testing => testing
 training_percents = {"20%" => 20, "40%" => 40, "60%" => 60, "80%" => 80}
 
 all_percents = training_percents.values.collect do |training_percent|
-  (1..10).to_a.inject do |average, trials|
+  (1..10).to_a.inject([]) do |accuracy_values, trials|
     training_count = (dataset.entries.length * training_percent) / 100
     
     shuffled_entries = dataset.entries.shuffle.shuffle.shuffle #just for fun
@@ -30,6 +29,9 @@ all_percents = training_percents.values.collect do |training_percent|
 
     result_total = {true => 0, false => 0}
 
+    puts
+    puts "Running test " + trials.to_s + " of 10 for " + training_percent.to_s + "% training set."
+
     testing_dataset.entries.each do |entry|
       result = test_decision_tree.matches_tree?(entry)
       if result then print '+' else print '.' end
@@ -39,13 +41,18 @@ all_percents = training_percents.values.collect do |training_percent|
     puts
     
     percent_accurate = result_total[true].to_f / testing_dataset.entries.length.to_f
-    average = ((average*(trials-1)) + percent_accurate) / trials
+    accuracy_values.push(percent_accurate.round(2))
   end
 end
 
-puts
+all_averages = all_percents.collect do |percent_set|
+  percent_set.inject(:+) / percent_set.length
+end
 
-final_results = Hash[training_percents.keys.zip(all_percents)]
-final_results.each do |label, percentage|
+final_percents = Hash[training_percents.keys.zip(all_percents)]
+final_averages = Hash[training_percents.keys.zip(all_averages)]
+final_averages.each do |label, percentage|
   puts label + " of data as training set- " + (percentage * 100).round(2).to_s + "% accurate."
+  puts "  Runs (1..10) accuracies: " + final_percents[label].to_s 
+  puts
 end
